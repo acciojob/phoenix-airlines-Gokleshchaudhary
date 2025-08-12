d06812f155227c0e6c0497ed7728f9f466facebf
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-12 10:52:24 +0530

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to Phoenix Airlines</h1>
+      <p>Book your flights easily!</p>
+    </div>
+  );
+}
+
+export default Landing;
 
